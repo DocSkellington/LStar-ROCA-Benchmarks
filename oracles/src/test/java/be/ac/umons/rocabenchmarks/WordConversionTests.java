7b4f2bff9b43d32674392248bccc23d49df9be0b
Gaëtan Staquet <gaetan.staquet@gmail.com>
2021-09-06 14:18:12 +0200

    Correction of unit tests for JSON simplification

--- oracles/src/test/java/be/ac/umons/rocabenchmarks/WordConversionTests.java
+++ oracles/src/test/java/be/ac/umons/rocabenchmarks/WordConversionTests.java
@@ -12,20 +12,20 @@ public class WordConversionTests {
     public void fromStringToSymbols() {
         String base = "hello";
         Word<JSONSymbol> result = WordConversion.fromStringToJSONSymbolWord(base);
-        Word<JSONSymbol> target = Word.fromSymbols(toSymbol("h"), toSymbol('e'), toSymbol('l'), toSymbol('l'), toSymbol('o'));
+        Word<JSONSymbol> target = Word.fromSymbols(toSymbol("hello"));
         Assert.assertEquals(result, target);
 
         base = "\"pro\":{\"i\":1}";
         result = WordConversion.fromStringToJSONSymbolWord(base);
         System.out.println(result);
-        target = Word.fromSymbols(toSymbol('"'), toSymbol('p'), toSymbol('r'), toSymbol('o'), toSymbol("\":{"), toSymbol('"'), toSymbol('i'), toSymbol("\":"), toSymbol('1'), toSymbol('}'));
+        target = Word.fromSymbols(toSymbol('"'), toSymbol("pro"), toSymbol("\""), toSymbol(":{"), toSymbol('"'), toSymbol('i'), toSymbol('"'), toSymbol(":"), toSymbol('1'), toSymbol('}'));
         Assert.assertEquals(result, target);
         
         // We ignore some spaces
         base = " \"pro\" : { \"i\" : 1 } ";
         result = WordConversion.fromStringToJSONSymbolWord(base);
         System.out.println(result);
-        target = Word.fromSymbols(toSymbol('"'), toSymbol('p'), toSymbol('r'), toSymbol('o'), toSymbol("\":{"), toSymbol('"'), toSymbol('i'), toSymbol("\":"), toSymbol('1'), toSymbol('}'));
+        target = Word.fromSymbols(toSymbol('"'), toSymbol("pro"), toSymbol('"'), toSymbol(":{"), toSymbol('"'), toSymbol('i'), toSymbol('"'), toSymbol(":"), toSymbol('1'), toSymbol('}'));
         Assert.assertEquals(result, target);
     }
 
