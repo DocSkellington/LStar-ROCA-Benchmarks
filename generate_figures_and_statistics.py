311077f2da008abe27be3c84914a8ff2486f10e1
Gaëtan Staquet <gaetan.staquet@gmail.com>
2021-09-22 17:01:07 +0200

    Computing mean and median total time

--- generate_figures_and_statistics.py
+++ generate_figures_and_statistics.py
@@ -1,7 +1,6 @@
 #!/bin/env python3
 
-# TODO: add counters for # of closedness and inconsistencies
-# Add column for size of alphabet after VCA transformation
+import sys
 
 import pandas
 import numpy as np
@@ -9,53 +8,36 @@ from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-df = pandas.read_csv("benchmarks/Results/Random/Random.csv")
+def save_to_file(filename: str, X, Y, Z):
+    with open(filename, "w") as f:
+        for y in range(len(Y)):
+            for x in range(len(X)):
+                f.write(f"{X[x][y]} {Y[x][y]} {Z[x][y]}\n")
+            f.write("\n")
 
-print(df.head())
+filepath = sys.argv[1]
+
+df = pandas.read_csv(filepath)
 
 column = "Total time (ms)"
 time = df[["Target ROCA size", "Alphabet size", column]]
-print(type(time["Target ROCA size"]))
 time = time.replace(to_replace="Timeout", value=60*10)
 time = time.drop(time[time[column] == "Error"].index)
 time[column] = pandas.to_numeric(time[column])
+time[column] = time[column].transform(lambda x: x / 1000)
 
-time_for_1_2 = time.loc[(time["Target ROCA size"] == 1) | (time["Target ROCA size"] == 2)]
-# To seconds
-# time[column] = time[column].transform(lambda x: x / 1000)
-
-print("Timeout:", time[time[column] == "Timeout"])
-print("Error:", time[time[column] == "Error"])
-
-dfGrouped = time_for_1_2.groupby(by=["Target ROCA size", "Alphabet size"])
+dfGrouped = time.groupby(by=["Target ROCA size", "Alphabet size"])
 means = dfGrouped.mean()
 medians = dfGrouped.median()
-print("Mean", means.head())
-print("Median", medians)
 
 original_X = [x[0] for x in dfGrouped.groups.keys()]
 original_Y = [x[1] for x in dfGrouped.groups.keys()]
-print(original_X, original_Y)
 X = np.linspace(min(original_X), max(original_X), len(original_X))
 Y = np.linspace(min(original_Y), max(original_Y), len(original_Y))
-print(X, Y)
 X, Y = np.meshgrid(X, Y)
-print(X, Y)
 
 Z_mean = griddata((original_X, original_Y), means[column], (X, Y), method="cubic")
-Z_mean[X > 2] = np.nan
 Z_median = griddata((original_X, original_Y), medians[column], (X, Y), method="cubic")
 
-fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
-surf_mean = ax.plot_surface(X, Y, Z_mean, cmap=cm.Blues, linewidth=0, antialiased=False)
-# surf_median = ax.plot_surface(X, Y, Z_median, cmap=cm.Reds, linewidth=0, antialiased=False)
-
-# ax.set_xlabel("Target ROCA size")
-# ax.set_ylabel("Alphabet size")
-# ax.set_zlabel("Time (ms)")
-# ax.set_title("Mean")
-plt.show()
-plt.close(fig)
-
-fig, ax = plt.subplots()
+save_to_file("mean_total_time.dat", X, Y, Z_mean)
+save_to_file("median_total_time.dat", X, Y, Z_median)
